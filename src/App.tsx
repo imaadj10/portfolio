931c425460b7a46983099439836944b93de77017
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2023-08-17 23:13:00 -0700

    Attemptiing basic setup of three js elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { useFrame, ThreeElements } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 
-function Box(props: ThreeElements['mesh']) {
-  const meshRef = useRef<THREE.Mesh>(null!)
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+function App() {
   return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'blue' : 'orange'} />
-    </mesh>
+    <div id="canvas-container">
+      <Canvas>
+
+      </Canvas>
+    </div>
   )
 }
 
-export default Box;
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Box from './App';
-import { Canvas } from '@react-three/fiber';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>,
+  <App />
 );
