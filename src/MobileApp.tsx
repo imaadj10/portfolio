2a18fbdabdcfdb2f59eff2207da618e151ef2e67
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2023-08-24 13:13:42 -0700

    Added note that mobile version is still a WIP

--- src/MobileApp.tsx
+++ src/MobileApp.tsx
@@ -1,7 +1,9 @@
 function MobileApp() {
-
   return (
-    <h1 style={{color: 'white'}}>Hello, welcome to my mobile website!</h1>
+    <>
+      <h1 style={{ color: 'white' }}>Hello, welcome to my mobile website!</h1>
+      <p style={{ color: 'white' }}>It's still a WIP, please checkout the desktop version.</p>
+    </>
   );
 }
 
