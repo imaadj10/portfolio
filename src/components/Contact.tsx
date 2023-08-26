3fa2419f9d6286b34e59be841710de7c7cdea70a
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2023-08-26 14:54:54 -0700

    contact page done without responsiveness

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,16 +10,19 @@ import {
   createStyles,
   getStylesRef,
 } from '@mantine/core';
+import { IconBrandLinkedin, IconMailFilled } from '@tabler/icons-react';
 
 const useStyles = createStyles((theme) => ({
   card: {
     backgroundColor: theme.colors.dark[8],
-    height: '85vh',
+    height: '52vh',
     width: '35%',
     left: '55%',
+    top: 155,
     [theme.fn.smallerThan('xl')]: {
       width: '40%',
       left: '55%',
+      top: 100,
     },
   },
 
@@ -42,12 +45,7 @@ const useStyles = createStyles((theme) => ({
 }));
 
 function Contact() {
-  return (
-    <ContactCard
-      name={'Hi, I am Imaad'}
-      description={'What is up'}
-    />
-  );
+  return <ContactCard name={'Hi, I am Imaad'} description={'What is up'} />;
 }
 
 interface ContactCardProps {
@@ -55,24 +53,74 @@ interface ContactCardProps {
   description: string;
 }
 
-function ContactCard({
-  name,
-  description,
-}: ContactCardProps) {
+function ContactCard({ name, description }: ContactCardProps) {
   const { classes } = useStyles();
 
   return (
     <Card withBorder radius="md" p="md" className={classes.card}>
       <Card.Section className={classes.section} mt="md">
-        <Group>
-          <Text fz="lg" fw={500}>
-            {name}
+        <Group position="center">
+          <Text style={{ fontSize: '36px' }} mt="md" fw={500}>
+            Reach out to me!
           </Text>
-          {/* <Badge size="sm">{year}</Badge> */}
         </Group>
-        <Text fz="sm" mt="xs">
-          {description}
-        </Text>
+        <Flex direction="column" align={'center'} gap="xl">
+          <Flex direction="column" align="center">
+            <Text>Follow me on GitHub:</Text>
+            <Button
+              size="xs"
+              color="dark"
+              radius="md"
+              variant="default"
+              component="a"
+              href={'https://www.github.com/imaadj10/'}
+              target="_blank"
+              title="Code"
+            >
+              <Image
+                src={'images/github.png'}
+                height="20px"
+                radius="xl"
+                fit="contain"
+              />
+              <Text ml="xs">imaadj10</Text>
+            </Button>
+          </Flex>
+
+          <Flex direction="column" align="center">
+            <Text>Connect with me on LinkedIn:</Text>
+            <Button
+              size="xs"
+              color="blue"
+              radius="md"
+              variant="filled"
+              component="a"
+              href={'https://www.linkedin.com/in/imaad-junaidi/'}
+              target="_blank"
+              title="Code"
+            >
+              <IconBrandLinkedin />
+              <Text ml="xs">Imaad Junaidi</Text>
+            </Button>
+          </Flex>
+
+          <Flex direction="column" align="center">
+            <Text>Send me an email:</Text>
+            <Button
+              size="xs"
+              color="green"
+              radius="md"
+              variant="filled"
+              component="a"
+              href={'mailto:imaadjunaidi1011@gmail.com'}
+              target="_blank"
+              title="Code"
+            >
+              <IconMailFilled />
+              <Text ml="xs">imaadjunaidi1011@gmail.com</Text>
+            </Button>
+          </Flex>
+        </Flex>
       </Card.Section>
     </Card>
   );
--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -135,7 +135,7 @@ function SolarSystem() {
       <div
         style={{
           opacity: !moving ? 0 : 1,
-          transition: 'opacity 0.5s ease',
+          transition: 'opacity 1s ease',
         }}
         class="repo-button"
       >
