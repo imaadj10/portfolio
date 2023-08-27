95f0296527025586379bfd0b2ba3f2cfbed6603d
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2023-08-26 23:06:49 -0700

    finished writing about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -30,8 +30,16 @@ function AboutCard() {
       </Flex>
 
       <Card.Section className={classes.section} mt="md">
-        <Text fz="md" mt="xs">
-          {'I am currently a third year Computer Science student at the University of British Columbia.'}
+        <Text fz="sm" mt="xs">
+          {`I'm from Peoria, Illinois, and am currently a third year Computer Science student at the University of British Columbia.
+            When I took my first introductory programming course in school, I instantly fell in love with the field.
+            I knew that the combination of intense problem solving and creative freedom characteristic of software engineering
+            was perfect for me. Now I spend my days working on projects, learning new technologies, and just enjoying
+            everything programming has to offer.`}<br/><br/>
+            {`When I'm not typing away at a computer I love to play the drums, hyperanalyze TV shows (I could talk for days
+              about Succession), and, if you couldn't tell by the website, learn about space.`}<br/><br/>
+            {`I'm always looking for new internship opportunities, so please feel free to message me if you're interested, or if
+            you'd just like to chat!`}
         </Text>
       </Card.Section>
     </Card>
