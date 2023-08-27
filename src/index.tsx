1eb4840f0e118ddf709893296bd83630a343b7a7
Imaad Junaidi <imaadjunaidi1011@gmail.com>
2023-08-27 12:56:40 -0700

    removed isMobile check

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
-import { isMobile } from 'react-device-detect';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import MobileApp from './MobileApp';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
